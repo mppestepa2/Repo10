e962403941c8dabb440fbf2f2b3749312a65fd77
mppestepa2 <36259583+mppestepa2@users.noreply.github.com>
2018-03-05 09:13:17 +0100

    Update HolaMundo.java

--- RepoJava10/src/HolaMundo.java
+++ RepoJava10/src/HolaMundo.java
@@ -4,7 +4,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
-		System.out.println("Hasta luego");
+		System.out.println("Hasta luego");		
+		System.out.println("Adios");
 	}
 
 }
